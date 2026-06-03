21874b927820037a2d4b065516820ce513ee04d3
Musoftware <mahmoudmn810@gmail.com>
2026-06-03 21:57:04 +0300

    feat: implement voucher system with management service, controller, and localized interfaces

---
+++ lang/en/vouchers.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'title' => 'Vouchers & Rewards',
+    'subtitle' => 'Redeem your vouchers and claim rewards when charging your wallet.',
+    'add_balance' => 'Add Balance',
+    'how_vouchers_work' => [
+        'title' => 'How Vouchers Work',
+        'description' => 'Vouchers provide you with extra reward balance when you charge your wallet. Once you meet the spend requirement of a voucher, the reward will automatically be added to your account during the checkout process.',
+    ],
+    'fixed' => 'Fixed Amount',
+    'spend' => 'Spend',
+    'get' => 'Get',
+    'valid_until' => 'Valid until',
+    'no_expiry' => 'No Expiry Date',
+    'max' => 'Max',
+    'per_user' => 'per user',
+    'add_balance_and_pay' => 'Add Balance & Pay',
+    'uses' => 'uses',
+    'limit' => 'Limit',
+    'empty' => [
+        'title' => 'No active vouchers',
+        'description' => 'There are no active vouchers available at the moment. Please check back later for new offers.',
+    ],
+    'redemptions' => [
+        'title' => 'Redemption History',
+        'spent' => 'Amount Spent',
+        'reward_received' => 'Reward Received',
+        'date' => 'Date',
+    ],
+    'voucher' => 'Voucher',
+];
