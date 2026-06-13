8198d89965c13b7039e54b6521ff65db4329de89
Musoftware <mahmoudmn810@gmail.com>
2026-06-13 18:54:39 +0300

    feat: implement core CRM modules, project and contract management, and multilingual localization support

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+
+class SetLocale
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->has('lang')) {
+            $lang = $request->get('lang');
+            if (in_array($lang, ['en', 'ar'])) {
+                Session::put('locale', $lang);
+            }
+        }
+
+        $locale = Session::get('locale', config('app.locale'));
+        App::setLocale($locale);
+
+        return $next($request);
+    }
+}
